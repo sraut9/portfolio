5b396e70a5c1002f21b732305d28de6d79d8604c
Shekhar Raut <sraut9@yahoo.com>
2021-03-25 17:25:07 +0530

    sequelize is working and mongo db using cloud now

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,6 +3,8 @@ const config = require('./config');
 module.exports = config.db;
 // this is being used...
 module.exports = {
-  url: "mongodb://localhost:27017/bezkoder_db"
+  // local
+  //url: "mongodb://localhost:27017/bezkoder_db"
+  // remote
+  url: "mongodb+srv://mongo_prod:pkpb1Vma3HeNYWD6@cluster0.0yygi.mongodb.net/portfolio?retryWrites=true&w=majority"
 };
-
