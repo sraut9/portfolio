b7fa0f655c472f71de2745f88cc4b3ba65dc6d26
Shekhar Raut <sraut9@yahoo.com>
2021-03-26 15:37:42 +0530

    merge?

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -7,10 +7,5 @@ const URL = process.env.DB_URL || "mongodb://localhost:27017/bezkoder_db";
 console.log("URL: " + URL);
 
 module.exports = {
-<<<<<<< HEAD
   url: URL 
-=======
-  // local
-  url: "mongodb://localhost:27017/bezkoder_db"
->>>>>>> master
 };
