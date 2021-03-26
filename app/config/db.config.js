a2340e62c283fad504dbe0759b924e62f809fb3e
Shekhar Raut <sraut9@yahoo.com>
2021-03-26 12:16:32 +0530

    db url env var for heroku

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,11 @@
 const config = require('./config');
+var AWS = require("aws-sdk");
 
 module.exports = config.db;
 // this is being used...
 module.exports = {
   // local
-  url: "mongodb://localhost:27017/bezkoder_db"
+  // url: "mongodb://localhost:27017/bezkoder_db"
+  // remote
+  url: process.env.DB_URL 
 };
